fb4912849b2ee6b9bdc0858f696220804d9646a5
PROPHET2350 <jorgeluis33ariosa@gmail.com>
2024-06-07 09:22:31 -0500

    feature add cors config

---
+++ src/main/java/com/contable/server/config/security/CORSConfig.java
@@ -0,0 +1,21 @@
+package com.contable.server.config.security;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableAutoConfiguration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigured() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+            }
+        };
+    }
+}
